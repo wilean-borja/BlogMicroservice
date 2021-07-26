8f2215a61467af55c7e5f6d4eef57dde7111c02e
wileanmacalaladsqreem <wilean.macalalad@sqreem.com>
2021-07-26 19:05:47 +0800

    using query service to get post and comments

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,13 +10,19 @@ app.use(
 );
 
 app.post("/events", (req, res) => {
-  events = req.body;
+  const event = req.body;
 
-  axios.post("http://localhost:4000/events", events);
-  axios.post("http://localhost:4001/events", events);
-  axios.post("http://localhost:4002/events", events);
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
 
-  res.send({ status: "OK " });
+  res.send({ status: "OK" });
 });
 
 app.listen(4005, () => {
